cd5255e3ec7b8ef8351f296a7174990f0262dfe2
Daniele Pelagatti <daniele@mono-grid.com>
2018-04-30 13:35:42 +0200

    updated logic so that chunksPath can be specified, updated readme file

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # threejs-glsl-loader
 A Webpack Loader that allows to load threejs inlined GLSL chunks.
+
+ALPHA VERSION, Tested with Threejs r92 
+
+## Webpack Configuration
+
+``` js
+module: {
+  rules: [{
+    test: /\.(glsl|vert|frag)$/,
+    loader: 'threejs-glsl-loader',
+    // Default values (can be omitted)
+    options: {
+      chunksPath: '../ShaderChunk', // if chunk fails to load with provided path (relative), the loader will retry with this one before giving up
+      chunksExt: 'glsl', // Chunks extension, used when #import statement omits extension
+    }
+  }]
+}
+```
+
+## How it behaves
+
+``` glsl
+// The content of chunks/a-chunk.glsl file will be inlined here as string
+// the loader will first try to load from the specified path (relative to the current glsl file)
+#include chunks/a-chunk.glsl;
+
+// this is how threejs specifies its chunks in its .glsl files
+// the loader will ignore <> characters and append chunksExt
+// the loader will then try to load the chunk from the same folder as the current glsl file
+// if this fails it will try to load the chunk from the specified chunksPath config argument
+// the default '../ShaderChunk' value for chunksPath currently (three.js r92) maps to where chunks are located in three.js
+#include <a-chunk>;
+
+```
--- index.js
+++ index.js
@@ -3,9 +3,8 @@ const path = require('path');
 const loaderUtils = require("loader-utils");
 const replaceAsync = require("string-replace-async");
 
-const DEFAULT_ROOT_PATH = '/';
 const DEFAULT_CHUNKS_EXT = 'glsl';
-const DEFAULT_VAR_PREFIX = '$';
+const DEFAULT_CHUNKS_PATH = '../ShaderChunk';
 
 function resolveDependency(loader, context, chunkPath) {
   return new Promise((resolve, reject) => {
@@ -28,7 +27,7 @@ function readFile(filePath, options) {
   });
 }
 
-function transformChunks(source, {rootPath, chunksExt}, loader) {
+function transformChunks(source, {chunksPath, chunksExt}, loader) {
   loader.cacheable();
   const callback = loader.async();
 
@@ -37,47 +36,37 @@ function transformChunks(source, {rootPath, chunksExt}, loader) {
 
     replaceAsync(source,/#include (.*);/ig, (match, includePath) => {
       includePath = './' + includePath.trim().replace(/;|<|>|.\//ig, '');
-      // console.log(includePath)
 
       const ext = path.extname(includePath);
       if(!ext) includePath = `${includePath}.${chunksExt}`;
 
-      const isAbsolute = path.isAbsolute(includePath);
+      const context = path.dirname(loader.resource)
 
-      const context = isAbsolute ? path.resolve(rootPath) : path.dirname(loader.resource);
-
-      if (isAbsolute) includePath = `.${includePath}`;
-
-      // console.log('\n\nTRYING FIRST TIME')
       return resolveDependency(loader, context, includePath)
         .catch(err => {
-          // console.log('\n\nFAILED')
+          // catch this, we'll retry with chunkPath
         })
         .then(chunkPath => {
           if (chunkPath) {
-            // console.log('\n\nSUCCESS')
+            // load was successful
             loader.addDependency(chunkPath);
             return readFile(chunkPath, 'utf-8');
           }
-
-          // console.log('\n\nTRYING SECOND TIME')
-          return resolveDependency(loader, path.resolve(path.dirname(loader.resource), '../ShaderChunk'), includePath)
+          // no cigar, retry chunkPath
+          return resolveDependency(loader, path.resolve(context, chunksPath), includePath)
             .then(chunkPath => {
-              // console.log('\n\nSUCCESS SECOND TIME!')
+              // success
               loader.addDependency(chunkPath);
               return readFile(chunkPath, 'utf-8');
             })
         })
         .then(file => {
-          // console.log('\n\nRETURNING READ FILE')
           return file.content
         });
       })
       .then(res => {
-        // console.log(`\n\nmodule.exports = \`${res}\`;`);
         callback(null, `module.exports = \`${res}\`;`)
       }).catch(err => {
-        // console.log(`\n\nERROR ${err}`);
         callback(err)
       })
   } else {
@@ -88,7 +77,7 @@ function transformChunks(source, {rootPath, chunksExt}, loader) {
 
 module.exports = function(source) {
   const options = loaderUtils.getOptions(this) || {}
-  const rootPath = (options.glsl && options.glsl.rootPath) || DEFAULT_ROOT_PATH;
-  const chunksExt = (options.glsl && options.glsl.chunksExt) || DEFAULT_CHUNKS_EXT;
-  transformChunks(source, {rootPath, chunksExt}, this);
+  const chunksPath = options.chunksPath || DEFAULT_CHUNKS_PATH;
+  const chunksExt = options.chunksExt || DEFAULT_CHUNKS_EXT;
+  transformChunks(source, {chunksPath, chunksExt}, this);
 };
